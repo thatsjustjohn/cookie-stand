dd59be2be4120d9f954ad47068146c3167a619ed
John Winters <jhnwin@amazon.com>
2019-04-01 18:08:57 -0700

    editing app.js for dom and data pushing to sales.html

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var hours = ['6am','7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 
@@ -107,27 +109,49 @@ var alki = {
     }
   }
 };
-
+//Create store and push all the stores into the array
 var stores = [];
 stores.push(firstAndPike, seaTacAirport, seattleCenter, capitolHill, alki);
-var numStores = stores.length;
 //test stuff
+
+//This for loop goes through each store and does 2 things, populates the numbers for the cookies
+//As well as creates the appropriate HTML for storing the data into different ULs
 stores.forEach(function(store){
   store.calculateCookiesPerHour();
   console.log('Results: ' + store.sales.hourlyCookies);
+  //gets the salesStuff element in the code
+  var bodyEL = document.getElementById('salesStuff');
+  //creates 2 elements a P tag for the name of the location and a UL for that specific p tag
+  var pEL = document.createElement('p');
+  var ulEL = document.createElement('ul');
+  //this sets the ID of the UL to the stores name for getting that ID for writing LI later
+  ulEL.id = store.location;
+  //write the store name into the P tag
+  pEL.textContent = store.location;
+  //Appends these items to the DOM
+  pEL.appendChild(ulEL);
+  bodyEL.appendChild(pEL);
 });
 
-
-
-var ulEl = document.getElementById('cookieHours');
+//This function actually puts the information into the auto generated HTML from the previous loop
 stores.forEach(function(store){
+  //gets the elements that we want to add into by name as initiates the variables
+  var ulEl = document.getElementById(store.location);
   var numOfHours = store.sales.hourlyCookies.length;
+  var totalCookies = 0;
+  //This loop adds into all the list items to the underorder list
   for (var i = 0; i < numOfHours; i++) {
     // create a <li> element
     var liEl = document.createElement('li');
     // give it content
-    liEl.textContent = store.sales.hourlyCookies[i];
+    liEl.textContent = hours[i] + ': ' + store.sales.hourlyCookies[i] + ' Cookies';
     // put it in the DOM
     ulEl.appendChild(liEl);
+    // keep running total
+    totalCookies += store.sales.hourlyCookies[i];
   }
+  //last line addition for total
+  liEl = document.createElement('li');
+  liEl.textContent = 'Total: ' + totalCookies;
+  ulEl.appendChild(liEl);
 });
